0574717e659a4b281fd0e78751b3b4713b5ee948
EmilyNorman2000 <122606064+EmilyNorman2000@users.noreply.github.com>
2023-01-13 05:21:05 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php include 'action.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact Us</title>
+
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<nav class="navbar navbar-default navBar">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">My Website</a>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<?php if($msg != ''): ?>
+			<div class="alert <?php echo $msgClass; ?>"><?php echo $msg; ?></div> 
+		<?php endif; ?>
+		<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" name="name" class="form-control" value="<?php echo isset($_POST['name']) ? $name : ''; ?>">
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input type="text" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $email : ''; ?>">
+			</div>
+			<div class="form-group">
+				<label>Message</label>
+				<textarea name="message" class="form-control"><?php echo isset($_POST['message']) ? $message : ''; ?></textarea>
+			</div>
+			<br>
+			<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
+</body>
+</html>
